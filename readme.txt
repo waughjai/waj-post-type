c3ab7d54d6bdae4f130b9ca98652695d934a5a96
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-26 11:54:49 -0800

    Init

---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== WAJ Post Type WordPress Plugin ===
+Contributors: waughjai
+Tags: link, post type, auto-generate
+Requires at least: 4.9.8
+Tested up to: 4.9.8
+Stable tag: 1.1.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Simple plugin for easy generation o' new post types.
---
+++ waj-post-type.php
@@ -0,0 +1,14 @@
+<?php
+	/*
+	Plugin Name:  WAJ Post Type
+	Plugin URI:   https://github.com/waughjai/copyright-year
+	Description:  Simple plugin for easy generation o' new post types.
+	Version:      1.0.0
+	Author:       Jaimeson Waugh
+	Author URI:   https://www.jaimeson-waugh.com
+	License:      GPL2
+	License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+	Text Domain:  waj-post-types
+	*/
+
+	require_once( 'vendor/autoload.php' );
